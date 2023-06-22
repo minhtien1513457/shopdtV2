400e4ef1ff5a16961fa45d6749ed0f24e7a46a08
Tien Phan <minhtienit1997@gmail.com>
2023-06-22 10:15:22 +0700

    authencator

---
+++ authencator/src/main/java/com/example/authencator/AuthencatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.authencator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthencatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthencatorApplication.class, args);
+	}
+
+}
---
+++ authencator/src/test/java/com/example/authencator/AuthencatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authencator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthencatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
